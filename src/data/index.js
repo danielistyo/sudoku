f20a300db62ce5bf3cbb6b4bc977bfb18ab26dae
Daniel <danielistyo16@gmail.com>
2019-11-06 05:24:54 +0700

    add sample data

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+import emptyPuzzle from "./puzzle0.json";
+import puzzle1 from "./puzzle1.json";
+import puzzle2 from "./puzzle2.json";
+import puzzle3 from "./puzzle3.json";
+import solution1 from "./solution1.json";
+import solution2 from "./solution2.json";
+import solution3 from "./solution3.json";
+
+export default {
+  puzzles: [puzzle1, puzzle2, puzzle3, emptyPuzzle],
+  solutions: [solution1, solution2, solution3]
+};
